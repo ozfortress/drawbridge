8715b6f90ce0f9bf119250a97a5cf9f48a806abb
Shigbeard <4262327+Shigbeard@users.noreply.github.com>
2024-05-07 18:09:45 +1000

    feat: :construction: Begin work on module loading.
    This is probably not going to stay for very long, we need some sort of global namespace to insert modules into, so each module can communicate with each other. Discord.py has the concept of Cogs, we may leverage something like that.

--- app.py
+++ app.py
@@ -4,17 +4,50 @@ if __name__ != '__main__':
 import logging
 import os
 from dotenv import load_dotenv
+import subprocess
+
 
 load_dotenv()
 
 if not os.path.exists('logs'):
     os.makedirs('logs')
 logger = logging.getLogger(__name__)
-_version = '0.0.1'
+VERSION = '0.0.1'
+
+class Drawbridge: pass
 
 def main():
     logging.basicConfig(filename='logs/drawbridge.log', level=logging._nameToLevel[os.getenv('LOG_LEVEL', 'INFO')], format='%(asctime)s - %(name)s - %(levelname)s - %(message)s')
-    logger.info(f'Starting OZF Drawbridge v{_version}...')
+    logger.info(f'Starting OZF Drawbridge v{VERSION}...')
+    checkPackages()
+    loaded = loadModule('citadel')
+    if not loaded:
+        logger.error('Could not load required modules. Exiting...')
+        return
+    logger.info('OZF Drawbridge has started.')
+
+
+
+def checkPackages():
+    with open('requirements.txt', 'r') as file:
+        requiredPackages = file.read().splitlines()
+    installedPackages = subprocess.check_output(['pip', 'freeze']).decode('utf-8').splitlines()
+    missingPackages = [package for package in requiredPackages if package not in installedPackages]
+    if missingPackages:
+        logger.warning(f'The following packages are missing: {missingPackages}')
+        logger.warning('Installing missing packages...')
+        subprocess.check_call(['pip', 'install', *missingPackages])
+        logger.info('All missing packages have been installed.')
+    else:
+        logger.info('All packages are already installed.')
+
+def loadModule(module):
+    try:
+        __import__(f'modules.{module}', globals(), locals(), ['*'])
+    except ImportError:
+        logger.error(f'Could not load module {module}.')
+        return False
+    return True
 
 if __name__ == '__main__':
     main()
