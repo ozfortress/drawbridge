966797461a9008aa2bd814d91798a427d10a1f21
Shigbeard <4262327+Shigbeard@users.noreply.github.com>
2024-06-09 02:56:34 +1000

    refactor: More refactoring into modular files. Mostly untested

--- app.py
+++ app.py
@@ -61,56 +61,12 @@ async def on_ready():
         logger.debug(f'Synced command: {synced_command.name}')
 
 
-def generate_log(message : discord.Message, is_team : bool, match_id=0, log_type="CREATE", after : discord.Message=None):
-    log = {}
-    if is_team:
-        team = database.get_team_by_channel_id(message.channel.id)
-        if not team:
-            return None
-        log['match_id'] = 0
-        log['team'] = team['team_id']
-    else:
-        if not match_id:
-            return None
-        match = db.get_match_details(match_id)
-        teamsRoles = []
-        teamsRoles.append(match['team_home'].role_id)
-        teamsRoles.append(match['team_away'].role_id)
-        log['match_id'] = match_id
-
-        for teamRole in teamsRoles:
-            if teamRole in message.author.roles:
-                log['team'] = database.get_team_id_of_role(teamRole)
-                break
-        if 'team' not in log:
-            log['team'] = None # Admin or Caster?
-            #TODO: Handle Admins and Casters
-    log['user_id'] = message.author.id
-    log['user_name'] = message.author.name
-    log['user_nick'] = message.author.nick
-    log['user_avatar'] = message.author.display_avatar.url
-    log['message_id'] = message.id
-    log['message_content'] = message.content
-
-    if message.attachments:
-        log['message_additionals'] = ' '.join([attachment.url for attachment in message.attachments])
-    else:
-        log['message_additionals'] = ''
-    log['log_type'] = log_type # CREATE / DELETE / EDIT
-    log['log_timestamp'] = message.created_at
-    if log_type == "EDIT":
-        log['message_content'] = after.content
-        log['log_timestamp'] = after.edited_at
-    if log_type == "DELETE":
-        log['log_timestamp'] = int(time.time())
-    db.insert_log(log)
-    logger.debug(f'new log {message.author.name}#{message.author.discriminator} ({message.author.id}) - {log_type}')
 
 @client.event
 async def on_message(message : discord.Message):
     match_id = db.get_match_id_of_channel(message.channel.id)
     if match_id:
-        generate_log(message, False, match_id, "CREATE")
+        Drawbridge.Functions.generate_log(message, False, match_id, "CREATE")
     else:
         # Verify cache is up to date
         if teamchannel_cache['refreshAfter'] < time.time():
@@ -119,13 +75,13 @@ async def on_message(message : discord.Message):
                 teamchannel_cache['channels'][channel['team_channel']] = channel['team_id']
             teamchannel_cache['refreshAfter'] = time.time() + 3600*24
         if message.channel.id in teamchannel_cache['channels']:
-            generate_log(message, True, 0, "CREATE")
+            Drawbridge.Functions.generate_log(message, True, 0, "CREATE")
 
 @client.event
 async def on_message_edit(before : discord.Message, after : discord.Message):
     match_id = db.get_match_id_of_channel(after.channel.id)
     if match_id:
-        generate_log(before, False, match_id, "EDIT", after)
+        Drawbridge.Functions.generate_log(before, False, match_id, "EDIT", after)
     else:
         # Verify cache is up to date
         if teamchannel_cache['refreshAfter'] < time.time():
@@ -134,14 +90,14 @@ async def on_message_edit(before : discord.Message, after : discord.Message):
                 teamchannel_cache['channels'][channel['team_channel']] = channel['team_id']
             teamchannel_cache['refreshAfter'] = time.time() + 3600*24
         if after.channel.id in teamchannel_cache['channels']:
-            generate_log(before, True, 0, "EDIT", after)
+            Drawbridge.Functions.generate_log(before, True, 0, "EDIT", after)
 
 @client.event
 async def on_message_delete(message : discord.Message):
     # WARNING - UNRELIABLE - MIGHT MISS OLD MSGS if they arent in cache.
     match_id = db.get_match_id_of_channel(message.channel.id)
     if match_id:
-        generate_log(message, False, match_id, "DELETE")
+        Drawbridge.Functions.generate_log(message, False, match_id, "DELETE")
     else:
         # Verify cache is up to date
         if teamchannel_cache['refreshAfter'] < time.time():
@@ -150,113 +106,7 @@ async def on_message_delete(message : discord.Message):
                 teamchannel_cache['channels'][channel['team_channel']] = channel['team_id']
             teamchannel_cache['refreshAfter'] = time.time() + 3600*24
         if message.channel.id in teamchannel_cache['channels']:
-            generate_log(message, True, 0, "DELETE")
-
-# @client.check
-# async def globally_block_dms(ctx):
-#     return ctx.guild is not None # Only allow commands to be run in a guild
-
-@Drawbridge.Checks.heads_only()
-@cmds.command(
-    name='get-teams',
-    guild=discord.Object(id=os.getenv('DISCORD_GUILD_ID'))
-)
-async def get_teams(interaction : discord.Interaction, league_id : int, league_shortcode : str, is_hl : bool = False):
-    """Generate team roles and channels for a given league
-
-    Parameters
-    -----------
-    league_id: int
-        The League ID to generate teams for
-    league_shortcode: str
-        The Shortcode for this league (eg: HL 27, 6s 30 ), this will be appended to role names.
-    is_hl: bool
-        Whether the league is Highlander or not. Default is False.
-    """
-    await interaction.response.send_message('Generating teams...', ephemeral=True)
-    league = cit.getLeague(league_id)
-    rosters = league.rosters
-    divs = []
-    file = open('embeds/teams.json', 'r')
-    for roster in rosters:
-        if roster['division'] not in divs:
-            divs.append(roster['division'])
-        # trim team name to 20 char
-        roster['role_name'] = f'{roster['name'][:20]} ({league_shortcode})'
-        # role = await ctx.guild.create_role(name=role_name)
-        # db.insert_team(roster)
-
-    await interaction.response.edit_message(content=f'Generating Division Categories, Team Channels, and Roles.\nLeague: {league['name']}\nDivisions: {len(divs)}\nTeams: {len(rosters)}')
-    r=0 #counters
-    d=0 #counters
-    for div in divs:
-        d+=1
-        catoverwrites = {
-            interaction.guild.default_role: discord.PermissionOverwrite(read_messages=False),
-            # discord.Object(id=(is_hl == True ? roles['HL Admin'] : roles['6s Admin'])) : discord.PermissionOverwrite(read_messages=True),
-            discord.Object(id=Drawbridge.Checks.roles['6s Head']) : discord.PermissionOverwrite(read_messages=True),
-            discord.Object(id=Drawbridge.Checks.roles['HL Head']) : discord.PermissionOverwrite(read_messages=True),
-            discord.Object(id=Drawbridge.Checks.roles['Trial Admin']) : discord.PermissionOverwrite(read_messages=True), # Or false? dunno hey.
-            discord.Object(id=Drawbridge.Checks.roles['Developers']) : discord.PermissionOverwrite(read_messages=True),
-            discord.Object(id=Drawbridge.Checks.roles['Approved Casters']) : discord.PermissionOverwrite(read_messages=False),
-            discord.Object(id=Drawbridge.Checks.roles['Unapproved Casters']) : discord.PermissionOverwrite(read_messages=False),
-            discord.Object(id=Drawbridge.Checks.roles['Captains Bot']) : discord.PermissionOverwrite(read_messages=True)
-        }
-        if is_hl:
-            catoverwrites[discord.Object(id=Drawbridge.Checks.roles['HL Admin'])] = discord.PermissionOverwrite(read_messages=True)
-        else:
-            catoverwrites[discord.Object(id=Drawbridge.Checks.roles['6s Admin'])] = discord.PermissionOverwrite(read_messages=True)
-
-        channelcategory = await interaction.guild.create_category(f'{div} - {league_shortcode}')
-        role = await interaction.guild.create_role(name=f'{div} - {league_shortcode}')
-        dbdiv = {
-            'division': div,
-            'role_id': role.id,
-            'category_id': channelcategory.id
-        }
-        divid = db.insert_div(dbdiv)
-        for roster in rosters:
-            if roster['division'] == div:
-                r+=1
-                role = await interaction.guild.create_role(name=f'{roster['name']} ({league_shortcode})', mentionable=True)
-                overwrites = {
-                    role: discord.PermissionOverwrite(read_messages=True),
-                }
-                teamchannel = await interaction.guild.create_text_channel(f'{roster['name']} ({league_shortcode})', category=channelcategory, overwrites=overwrites)
-
-                # Load the chat message from embeds/teams.json
-
-                teammessage = json.load(file)
-                teammessage = Drawbridge.Functions.substitute_strings_in_embed(teammessage, {
-                    '{TEAM_MENTION}': f'<@&{role.id}>',
-                    '{TEAM_NAME}': roster['name'],
-                    '{TEAM_ID}': roster['id'],
-                    '{DIVISION}': div,
-                    '{LEAGUE_NAME}': league['name'],
-                    '{LEAGUE_SHORTCODE}': league_shortcode,
-                    '{CHANNEL_ID}': str(teamchannel.id),
-                    '{CHANNEL_LINK}': f'<#{teamchannel.id}>',
-                })
-                teammessage['embed'] = teammessage['embeds'][0]
-                del teammessage['embeds']
-                await teamchannel.send(**teammessage)
-                await interaction.response.edit_message(content=f'Generating Division Categories, Team Channels, and Roles.\nLeague: {league['name']}\nDivisions: {d}/{len(divs)}\nTeams: {r}/{len(rosters)}')
-                dbteam = {
-                    'team_id': roster['id'],
-                    'role_id': role.id,
-                    'team_channel': teamchannel.id,
-                    'division': divid,
-                    'team_name': roster['name']
-                }
-                db.insert_team(dbteam)
-    await interaction.response.edit_message(content=f'Generated.\nLeague: {league['name']}\nDivisions: {d}/{len(divs)}\nTeams: {r}/{len(rosters)}')
-
-@get_teams.error
-async def get_teams_error(ctx : discord.Interaction, error):
-    if isinstance(error, commands.errors.CheckFailure):
-        await ctx.response.send_message(content='You do not have permission to run this command.', ephermeral=True)
-    else:
-        await ctx.response.send_message(content='An error occurred while running this command.', ephermeral=True)
+            Drawbridge.Functions.generate_log(message, True, 0, "DELETE")
 
 if __name__ == '__main__':
     main()
--- modules/Drawbridge/functions.py
+++ modules/Drawbridge/functions.py
@@ -1,8 +1,17 @@
+import modules.database as database
+import modules.citadel as citadel
+import discord
+import logging
+import time
+
 class Functions:
     """
     Drawbridge Helper Functions."""
 
-    def __init__(self):
+    def __init__(self, db : database.Database, cit : citadel.Citadel):
+        self.db = db
+        self.cit = cit
+        self.logger = logging.getLogger(__name__)
         pass
 
     def substitute_strings_in_embed(self, json: dict | list | str, substitutions: dict, depth : int = 0) -> dict | list | str:
@@ -44,3 +53,48 @@ class Functions:
             return [self.substitute_strings_in_embed(item, substitutions) for item in json]
         else:
             return json
+
+    def generate_log(self, message : discord.Message, is_team : bool, match_id=0, log_type="CREATE", after : discord.Message=None):
+        log = {}
+        if is_team:
+            team = database.get_team_by_channel_id(message.channel.id)
+            if not team:
+                return None
+            log['match_id'] = 0
+            log['team'] = team['team_id']
+        else:
+            if not match_id:
+                return None
+            match = self.db.get_match_details(match_id)
+            teamsRoles = []
+            teamsRoles.append(match['team_home'].role_id)
+            teamsRoles.append(match['team_away'].role_id)
+            log['match_id'] = match_id
+
+            for teamRole in teamsRoles:
+                if teamRole in message.author.roles:
+                    log['team'] = database.get_team_id_of_role(teamRole)
+                    break
+            if 'team' not in log:
+                log['team'] = None # Admin or Caster?
+                #TODO: Handle Admins and Casters
+        log['user_id'] = message.author.id
+        log['user_name'] = message.author.name
+        log['user_nick'] = message.author.nick
+        log['user_avatar'] = message.author.display_avatar.url
+        log['message_id'] = message.id
+        log['message_content'] = message.content
+
+        if message.attachments:
+            log['message_additionals'] = ' '.join([attachment.url for attachment in message.attachments])
+        else:
+            log['message_additionals'] = ''
+        log['log_type'] = log_type # CREATE / DELETE / EDIT
+        log['log_timestamp'] = message.created_at
+        if log_type == "EDIT":
+            log['message_content'] = after.content
+            log['log_timestamp'] = after.edited_at
+        if log_type == "DELETE":
+            log['log_timestamp'] = int(time.time())
+        self.db.insert_log(log)
+        self.logger.debug(f'new log {message.author.name}#{message.author.discriminator} ({message.author.id}) - {log_type}')
