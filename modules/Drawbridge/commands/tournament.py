0397976175a33770033e1160b7b6d8f50e90b827
Shigbeard <4262327+Shigbeard@users.noreply.github.com>
2024-07-01 21:21:48 +1000

    debug test

--- modules/Drawbridge/commands/tournament.py
+++ modules/Drawbridge/commands/tournament.py
@@ -439,8 +439,20 @@ class Tournament(discord_commands.GroupCog, group_name='tournament', name='tourn
                             rawlaunchpadmessage += f'- {team[3]} -> <#{team[4]}>\n'
                     rawlaunchpadmessage += f'## Matches\n'
                     for match in matches:
+                        c = 0
+                        if c==0:
+                            self.logger.debug(f'{match}')
+                            self.logger.debug(f'{div}')
+                            self.logger.debug(f'{leagues}')
+                            c+=1
                         if (match[6] == leagues.id) and (match[1] == div[0]):
-                            rawlaunchpadmessage += f'- [{match[0]}](<https://ozfortress.com/matches/{match[0]}>) -> <#{match[4]}>\n'
+                            if match[4] == 0:
+                                rawlaunchpadmessage += f'- [{match[0]}](<https://ozfortress.com/matches/{match[0]}>) -> Bye\n'
+                            else:
+                                rawlaunchpadmessage += f'- [{match[0]}](<https://ozfortress.com/matches/{match[0]}>) -> <#{match[4]}>\n'
+                            c+=1
+                        if c == 0:
+                            rawlaunchpadmessage += f'- No matches\n'
                     rawlaunchpadmessage += '\n'
         launchpadmessages = []
         # split on the first \n under 2000 chars
