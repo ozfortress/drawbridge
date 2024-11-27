1ce9b7126b72bd9f4fb78610474d5dace6391d5d
Hamish Luke <hamishrluke@gmail.com>
2024-11-27 18:47:42 +1100

    debugging: variables for self.db.get_team_by_id

--- modules/Drawbridge/commands/tournament.py
+++ modules/Drawbridge/commands/tournament.py
@@ -462,7 +462,7 @@ class Tournament(discord_commands.GroupCog, group_name='tournament', name='tourn
             }))
             team_channel = self.bot.get_channel(db_team[4])
             if team_channel is None:
-                await interaction.edit_original_response(content=f'Channel couldn\'t be found. db_team[4]:{db_team[4]}. Aborting.')
+                await interaction.edit_original_response(content=f'Channel couldn\'t be found. dbsize: {len(db_team)} db_team[0]:{db_team[0]}db_team[1]:{db_team[1]}db_team[2]:{db_team[2]}db_team[3]:{db_team[3]}. Aborting.')
                 return
             demochkmsg['embed'] = discord.Embed(**demochkmsg['embeds'][0])
             del demochkmsg['embeds']
