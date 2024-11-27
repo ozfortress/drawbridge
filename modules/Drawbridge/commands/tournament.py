47a905fd28baa24d5c3cf835d1c4447ebd460e19
Hamish Luke <hamishrluke@gmail.com>
2024-11-27 18:20:34 +1100

    fixed: incorrect variables

--- modules/Drawbridge/commands/tournament.py
+++ modules/Drawbridge/commands/tournament.py
@@ -443,7 +443,7 @@ class Tournament(discord_commands.GroupCog, group_name='tournament', name='tourn
             else:
                 chosen_team = chosem_match['away_team']
 
-            t = self.db.get_team_by_id(chosen_team["id"])
+            db_team = self.db.get_team_by_id(chosen_team['id'])
 
             messageraw = ''
             with open('embeds/democheck.json', 'r') as file:
@@ -451,17 +451,17 @@ class Tournament(discord_commands.GroupCog, group_name='tournament', name='tourn
             tempmsg = str(messageraw)
 
             demochkmsg = json.loads(self.functions.substitute_strings_in_embed(tempmsg, {
-                '{TEAM_NAME}'   : f'<@&{t['role_id']}>',
+                '{TEAM_NAME}'   : f'<@&{db_team[2]}>',
                 '{TARGET_NAME}' : f'{chosen_player['name']}',
                 '{TARGET_ID}'   : f'{chosen_player['id']}',
                 '{MATCH_PAGE}'  : f'tbd',
                 '{MATCH_ID}'    : f'tbd'
             }))
-            self.bot.get_channel(t['channel'])
+            team_channel = self.bot.get_channel(db_team[4])
             demochkmsg['embed'] = discord.Embed(**demochkmsg['embeds'][0])
             del demochkmsg['embeds']
-            await t['team'].send(**demochkmsg)
-            await interaction.edit_original_response(content=f'Random demo check announced. Player chosen is: {chosen_player.name}')
+            await team_channel.send(**demochkmsg)
+            await interaction.edit_original_response(content=f'Random demo check announced. Player chosen is: {chosen_player['name']}')
         except Exception as e:
             self.logger.error(f'Error conducting demo check: {e}', exc_info=True)
             await interaction.edit_original_response(content=f'An error occurred while announcing the random demo check. Error: {e}. Line {e.__traceback__.tb_lineno}.')
