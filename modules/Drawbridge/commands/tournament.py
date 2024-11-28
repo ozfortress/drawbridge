7235aa36d5a3f5410a360b3c3b94eed0315e1fec
Hamish Luke <hamishrluke@gmail.com>
2024-11-28 19:18:11 +1100

    debug: length of matches

--- modules/Drawbridge/commands/tournament.py
+++ modules/Drawbridge/commands/tournament.py
@@ -484,7 +484,7 @@ class Tournament(discord_commands.GroupCog, group_name='tournament', name='tourn
             await interaction.edit_original_response(content=f'Random demo check announced. Player chosen is: {player_chosen['name']}')
         except Exception as e:
             self.logger.error(f'Error conducting demo check: {e}', exc_info=True)
-            await interaction.edit_original_response(content=f'An error occurred while announcing the random demo check. Error: {e}. Line {e.__traceback__.tb_lineno}.')
+            await interaction.edit_original_response(content=f'An error occurred while announcing the random demo check. Error: {e}. Line {e.__traceback__.tb_lineno}. Potential match pool length = {len(matches)}')
 
     ''' I'm saving this logic for later - Ama
                 #This part can be removed to improve performance. Consult amatorii if you have questions
